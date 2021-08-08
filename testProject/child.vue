fe08510aaa6f96e9816df757422d2596bd5050de
storm-rage <361558338@qq.com>
2021-08-08 19:58:05 +0800

    dev分支
    上传代码到dev分支

---
+++ testProject/child.vue
@@ -0,0 +1,12 @@
+<template>
+    <h3>我是子组件</h3>
+</template>
+<script>
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+    setup() {
+        
+    },
+})
+</script>
---
+++ testProject/father.vue
@@ -0,0 +1,13 @@
+<template>
+    <h2>我是父组件</h2>
+    <child></child>
+</template>
+<script>
+import child from './child.vue'
+
+export default {
+  name:'father',
+  components: { child }
+}
+</script>
+
